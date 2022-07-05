58353bf1c0b291a02e1ceb736c725e7d0f661594
Supun Setunga <supun.setunga@gmail.com>
2022-07-05 09:27:28 -0700

    Add duplicate switch-case analyzer

--- analyzers/analyzers_test.go
+++ analyzers/analyzers_test.go
@@ -19,12 +19,16 @@
 package analyzers_test
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/cadence/runtime/ast"
 	"github.com/onflow/cadence/runtime/common"
+	"github.com/onflow/cadence/runtime/sema"
+	"github.com/onflow/cadence/runtime/tests/checker"
 	"github.com/onflow/cadence/tools/analysis"
 
 	"github.com/onflow/cadence-lint/analyzers"
@@ -37,8 +41,8 @@ func testAnalyzers(t *testing.T, code string, analyzers ...*analysis.Analyzer) [
 
 	config := analysis.NewSimpleConfig(
 		analysis.NeedTypes,
-		map[common.LocationID]string{
-			testLocationID: code,
+		map[common.Location]string{
+			testLocation: code,
 		},
 		nil,
 		nil,
@@ -49,7 +53,7 @@ func testAnalyzers(t *testing.T, code string, analyzers ...*analysis.Analyzer) [
 
 	var diagnostics []analysis.Diagnostic
 
-	programs[testLocationID].Run(
+	programs[testLocation].Run(
 		analyzers,
 		func(diagnostic analysis.Diagnostic) {
 			diagnostics = append(diagnostics, diagnostic)
@@ -603,5 +607,533 @@ func TestCheckNumberConversionReplacementHint(t *testing.T) {
 			},
 			diagnostics,
 		)
+	})
+}
+
+func TestCheckSwitchStatementDuplicateCases(t *testing.T) {
+
+	t.Parallel()
+
+	t.Run("multiple duplicates", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                let s: String? = nil
+                switch s {
+                    case "foo":
+                        return 1
+                    case "bar":
+                        return 2
+                    case "bar":
+                        return 3
+                    case "bar":
+                        return 4
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		// Should only report two errors.
+		// i.e: second and the third duplicate cases must not be compared with each other.
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 254, Line: 9, Column: 25},
+						EndPos:   ast.Position{Offset: 258, Line: 9, Column: 29},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 319, Line: 11, Column: 25},
+						EndPos:   ast.Position{Offset: 323, Line: 11, Column: 29},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+
+			diagnostics,
+		)
+	})
+
+	t.Run("simple literals", func(t *testing.T) {
+		type test struct {
+			name string
+			expr string
+		}
+
+		expressions := []test{
+			{
+				name: "string",
+				expr: "\"hello\"",
+			},
+			{
+				name: "integer",
+				expr: "5",
+			},
+			{
+				name: "fixedpoint",
+				expr: "4.7",
+			},
+			{
+				name: "boolean",
+				expr: "true",
+			},
+		}
+
+		for _, testCase := range expressions {
+
+			t.Run(testCase.name, func(t *testing.T) {
+				diagnostics := testAnalyzers(t,
+					fmt.Sprintf(`
+                        pub fun test(): Int {
+                            let x = %[1]s
+                            switch x {
+                                case %[1]s:
+                                    return 1
+                                case %[1]s:
+                                    return 2
+                            }
+                            return -1
+                        }`,
+						testCase.expr,
+					),
+					analyzers.SwitchCaseAnalyzer,
+				)
+
+				l := len(testCase.expr)
+
+				require.Equal(
+					t,
+					[]analysis.Diagnostic{
+						{
+							Range: ast.Range{
+								StartPos: ast.Position{Offset: 244 + 2*l, Line: 7, Column: 37},
+								EndPos:   ast.Position{Offset: 244 + 3*l - 1, Line: 7, Column: 37 + l - 1},
+							},
+							Location: testLocation,
+							Category: "lint",
+							Message:  "duplicate switch case",
+						},
+					},
+					diagnostics,
+				)
+			})
+		}
+	})
+
+	t.Run("identifier", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                let x = 5
+                let y = 5
+                switch 4 {
+                    case x:
+                        return 1
+                    case x:
+                        return 2
+                    case y:  // different identifier
+                        return 3
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 200, Line: 8, Column: 25},
+						EndPos:   ast.Position{Offset: 200, Line: 8, Column: 25},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("member access", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                let x = Foo()
+                switch x.a {
+                    case x.a:
+                        return 1
+                    case x.a:
+                        return 2
+                    case x.b:
+                        return 3
+                }
+                return -1
+            }
+            pub struct Foo {
+                pub var a: String
+                pub var b: String
+                init() {
+                    self.a = "foo"
+                    self.b = "bar"
+                }
+            }`,
+
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 182, Line: 7, Column: 25},
+						EndPos:   ast.Position{Offset: 184, Line: 7, Column: 27},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("index access", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                let x: [Int] = [1, 2, 3]
+                let y: [Int] = [5, 6, 7]
+                switch x[0] {
+                    case x[1]:
+                        return 1
+                    case x[1]:
+                        return 2
+                    case x[2]:
+                        return 3
+                    case y[1]:
+                        return 4
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 237, Line: 8, Column: 26},
+						EndPos:   ast.Position{Offset: 239, Line: 8, Column: 28},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("conditional", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                switch "foo" {
+                    case true ? "foo" : "bar":
+                        return 1
+                    case true ? "foo" : "bar":
+                        return 2
+                    case true ? "baz" : "bar":  // different then expr
+                        return 3
+                    case true ? "foo" : "baz":  // different else expr
+                        return 4
+                    case false ? "foo" : "bar":  // different condition expr
+                        return 5
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 171, Line: 6, Column: 25},
+						EndPos:   ast.Position{Offset: 190, Line: 6, Column: 44},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("unary", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                let x = 5
+                let y = x
+                switch x {
+                    case -x:
+                        return 1
+                    case -x:
+                        return 2
+                    case -y:  // different rhs expr
+                        return 3
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 201, Line: 8, Column: 25},
+						EndPos:   ast.Position{Offset: 202, Line: 8, Column: 26},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("binary", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                switch 4 {
+                    case 3+5:
+                        return 1
+                    case 3+5:
+                        return 2
+                    case 3+7:  // different rhs expr
+                        return 3
+                    case 7+5:  // different lhs expr
+                        return 4
+                    case 3-5:  // different operator
+                        return 5
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 150, Line: 6, Column: 25},
+						EndPos:   ast.Position{Offset: 152, Line: 6, Column: 27},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("cast", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                let x = 5
+                let y = x as Integer
+                switch y {
+                    case x as Integer:
+                        return 1
+                    case x as Integer:
+                        return 2
+                    case x as! Integer:  // different operator
+                        return 3
+                    case y as Integer:  // different expr
+                        return 4
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 222, Line: 8, Column: 25},
+						EndPos:   ast.Position{Offset: 233, Line: 8, Column: 36},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("create", func(t *testing.T) {
+		_, err := checker.ParseAndCheck(t, `
+            pub fun test() {
+                let x <- create Foo()
+                switch x {
+                }
+                destroy x
+            }
+            pub resource Foo {}
+        `)
+
+		errs := checker.ExpectCheckerErrors(t, err, 1)
+		assert.IsType(t, &sema.NotEquatableTypeError{}, errs[0])
+	})
+
+	t.Run("destroy", func(t *testing.T) {
+
+		_, err := checker.ParseAndCheck(t, `
+          pub fun test() {
+              let x <- create Foo()
+              switch destroy x {
+              }
+          }
+          pub resource Foo {}
+        `)
+
+		errs := checker.ExpectCheckerErrors(t, err, 1)
+		assert.IsType(t, &sema.NotEquatableTypeError{}, errs[0])
+	})
+
+	t.Run("reference", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                let x: Int = 5
+                let y: Int = 7
+                switch (&x as &Int) {
+                    case &x as &Int:
+                        return 1
+                    case &x as &Int:
+                        return 2
+                    case &y as &Int:  // different expr
+                        return 2
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 230, Line: 8, Column: 25},
+						EndPos:   ast.Position{Offset: 239, Line: 8, Column: 34},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("force", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                let x: Int? = 5
+                let y: Int? = 5
+                switch 4 {
+                    case x!:
+                        return 1
+                    case x!:
+                        return 2
+                    case y!:    // different expr
+                        return 3
+                }
+                return -1
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		require.Equal(
+			t,
+			[]analysis.Diagnostic{
+				{
+					Range: ast.Range{
+						StartPos: ast.Position{Offset: 213, Line: 8, Column: 25},
+						EndPos:   ast.Position{Offset: 214, Line: 8, Column: 26},
+					},
+					Location: testLocation,
+					Category: "lint",
+					Message:  "duplicate switch case",
+				},
+			},
+			diagnostics,
+		)
+	})
+
+	t.Run("path", func(t *testing.T) {
+		_, err := checker.ParseAndCheck(t, `
+            pub fun test() {
+                switch /public/somepath {
+                }
+            }
+        `)
+
+		errs := checker.ExpectCheckerErrors(t, err, 1)
+		assert.IsType(t, &sema.NotEquatableTypeError{}, errs[0])
+	})
+
+	t.Run("invocation", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                switch "hello" {
+                    case foo():
+                        return 1
+                    case foo():
+                        return 2
+                }
+                return -1
+            }
+
+            pub fun foo(): String {
+                return "hello"
+            }`,
+
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		assert.Empty(t, diagnostics)
+	})
+
+	t.Run("default", func(t *testing.T) {
+		diagnostics := testAnalyzers(t, `
+            pub fun test(): Int {
+                switch "hello" {
+                    default:
+                        return -1
+                }
+            }`,
+			analyzers.SwitchCaseAnalyzer,
+		)
+
+		assert.Empty(t, diagnostics)
 	})
 }
---
+++ analyzers/switch_case_analyzer.go
@@ -0,0 +1,348 @@
+/*
+ * Cadence-lint - The Cadence linter
+ *
+ * Copyright 2019-2022 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package analyzers
+
+import (
+	"github.com/onflow/cadence/runtime"
+	"github.com/onflow/cadence/runtime/ast"
+	"github.com/onflow/cadence/tools/analysis"
+)
+
+var _ ast.ExpressionVisitor = &DuplicateCaseChecker{}
+var _ ast.TypeEqualityChecker = &DuplicateCaseChecker{}
+
+type DuplicateCaseChecker struct {
+	runtime.TypeEqualityChecker
+	expr ast.Expression
+}
+
+func NewDuplicateCaseChecker(program *ast.Program) DuplicateCaseChecker {
+	rootDeclIdentifier := getRootDeclarationIdentifier(program)
+
+	return DuplicateCaseChecker{
+		TypeEqualityChecker: runtime.TypeEqualityChecker{
+			RootDeclIdentifier: rootDeclIdentifier,
+		},
+	}
+}
+
+func getRootDeclarationIdentifier(program *ast.Program) *ast.Identifier {
+	compositeDecl := program.SoleContractDeclaration()
+	if compositeDecl != nil {
+		return compositeDecl.DeclarationIdentifier()
+	}
+
+	interfaceDecl := program.SoleContractInterfaceDeclaration()
+	if interfaceDecl != nil {
+		return interfaceDecl.DeclarationIdentifier()
+	}
+
+	return nil
+}
+
+func (d *DuplicateCaseChecker) isDuplicate(this ast.Expression, other ast.Expression) bool {
+	if this == nil || other == nil {
+		return false
+	}
+
+	tempExpr := d.expr
+	d.expr = this
+	defer func() {
+		d.expr = tempExpr
+	}()
+
+	return other.AcceptExp(d).(bool)
+}
+
+func (d *DuplicateCaseChecker) VisitBoolExpression(otherExpr *ast.BoolExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.BoolExpression)
+	if !ok {
+		return false
+	}
+
+	return otherExpr.Value == expr.Value
+}
+
+func (d *DuplicateCaseChecker) VisitNilExpression(_ *ast.NilExpression) ast.Repr {
+	_, ok := d.expr.(*ast.NilExpression)
+	return ok
+}
+
+func (d *DuplicateCaseChecker) VisitIntegerExpression(otherExpr *ast.IntegerExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.IntegerExpression)
+	if !ok {
+		return false
+	}
+
+	return expr.Value.Cmp(otherExpr.Value) == 0
+}
+
+func (d *DuplicateCaseChecker) VisitFixedPointExpression(otherExpr *ast.FixedPointExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.FixedPointExpression)
+	if !ok {
+		return false
+	}
+
+	return expr.Negative == otherExpr.Negative &&
+		expr.Fractional.Cmp(otherExpr.Fractional) == 0 &&
+		expr.UnsignedInteger.Cmp(otherExpr.UnsignedInteger) == 0 &&
+		expr.Scale == otherExpr.Scale
+}
+
+func (d *DuplicateCaseChecker) VisitArrayExpression(otherExpr *ast.ArrayExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.ArrayExpression)
+	if !ok || len(expr.Values) != len(otherExpr.Values) {
+		return false
+	}
+
+	for index, value := range expr.Values {
+		if !d.isDuplicate(value, otherExpr.Values[index]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (d *DuplicateCaseChecker) VisitDictionaryExpression(otherExpr *ast.DictionaryExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.DictionaryExpression)
+	if !ok || len(expr.Entries) != len(otherExpr.Entries) {
+		return false
+	}
+
+	for index, entry := range expr.Entries {
+		otherEntry := otherExpr.Entries[index]
+
+		if !d.isDuplicate(entry.Key, otherEntry.Key) ||
+			!d.isDuplicate(entry.Value, otherEntry.Value) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (d *DuplicateCaseChecker) VisitIdentifierExpression(otherExpr *ast.IdentifierExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.IdentifierExpression)
+	if !ok {
+		return false
+	}
+
+	return expr.Identifier.Identifier == otherExpr.Identifier.Identifier
+}
+
+func (d *DuplicateCaseChecker) VisitInvocationExpression(_ *ast.InvocationExpression) ast.Repr {
+	// Invocations can be stateful. Thus, it's not possible to determine if
+	// invoking the same function in two cases would produce the same results.
+	return false
+}
+
+func (d *DuplicateCaseChecker) VisitMemberExpression(otherExpr *ast.MemberExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.MemberExpression)
+	if !ok {
+		return false
+	}
+
+	return d.isDuplicate(expr.Expression, otherExpr.Expression) &&
+		expr.Optional == otherExpr.Optional &&
+		expr.Identifier.Identifier == otherExpr.Identifier.Identifier
+}
+
+func (d *DuplicateCaseChecker) VisitIndexExpression(otherExpr *ast.IndexExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.IndexExpression)
+	if !ok {
+		return false
+	}
+
+	return d.isDuplicate(expr.TargetExpression, otherExpr.TargetExpression) &&
+		d.isDuplicate(expr.IndexingExpression, otherExpr.IndexingExpression)
+}
+
+func (d *DuplicateCaseChecker) VisitConditionalExpression(otherExpr *ast.ConditionalExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.ConditionalExpression)
+	if !ok {
+		return false
+	}
+
+	return d.isDuplicate(expr.Test, otherExpr.Test) &&
+		d.isDuplicate(expr.Then, otherExpr.Then) &&
+		d.isDuplicate(expr.Else, otherExpr.Else)
+}
+
+func (d *DuplicateCaseChecker) VisitUnaryExpression(otherExpr *ast.UnaryExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.UnaryExpression)
+	if !ok {
+		return false
+	}
+
+	return d.isDuplicate(expr.Expression, otherExpr.Expression) &&
+		expr.Operation == otherExpr.Operation
+}
+
+func (d *DuplicateCaseChecker) VisitBinaryExpression(otherExpr *ast.BinaryExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.BinaryExpression)
+	if !ok {
+		return false
+	}
+
+	return d.isDuplicate(expr.Left, otherExpr.Left) &&
+		d.isDuplicate(expr.Right, otherExpr.Right) &&
+		expr.Operation == otherExpr.Operation
+}
+
+func (d *DuplicateCaseChecker) VisitFunctionExpression(_ *ast.FunctionExpression) ast.Repr {
+	// Not a valid expression for switch-case. Hence, skip.
+	return false
+}
+
+func (d *DuplicateCaseChecker) VisitStringExpression(otherExpr *ast.StringExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.StringExpression)
+	if !ok {
+		return false
+	}
+
+	return expr.Value == otherExpr.Value
+}
+
+func (d *DuplicateCaseChecker) VisitCastingExpression(otherExpr *ast.CastingExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.CastingExpression)
+	if !ok {
+		return false
+	}
+
+	if !d.isDuplicate(expr.Expression, otherExpr.Expression) ||
+		expr.Operation != otherExpr.Operation {
+		return false
+	}
+
+	typ := expr.TypeAnnotation.Type
+	otherType := otherExpr.TypeAnnotation.Type
+	return typ.CheckEqual(otherType, d) == nil
+}
+
+func (d *DuplicateCaseChecker) VisitCreateExpression(_ *ast.CreateExpression) ast.Repr {
+	// Not a valid expression for switch-case. Hence, skip.
+	return false
+}
+
+func (d *DuplicateCaseChecker) VisitDestroyExpression(_ *ast.DestroyExpression) ast.Repr {
+	// Not a valid expression for switch-case. Hence, skip.
+	return false
+}
+
+func (d *DuplicateCaseChecker) VisitReferenceExpression(otherExpr *ast.ReferenceExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.ReferenceExpression)
+	if !ok {
+		return false
+	}
+
+	return d.isDuplicate(expr.Expression, otherExpr.Expression)
+}
+
+func (d *DuplicateCaseChecker) VisitForceExpression(otherExpr *ast.ForceExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.ForceExpression)
+	if !ok {
+		return false
+	}
+
+	return d.isDuplicate(expr.Expression, otherExpr.Expression)
+}
+
+func (d *DuplicateCaseChecker) VisitPathExpression(otherExpr *ast.PathExpression) ast.Repr {
+	expr, ok := d.expr.(*ast.PathExpression)
+	if !ok {
+		return false
+	}
+
+	return expr.Domain == otherExpr.Domain &&
+		expr.Identifier.Identifier == otherExpr.Identifier.Identifier
+}
+
+var SwitchCaseAnalyzer = (func() *analysis.Analyzer {
+
+	elementFilter := []ast.Element{
+		(*ast.SwitchStatement)(nil),
+	}
+
+	return &analysis.Analyzer{
+		Description: "Detects duplicate switch cases",
+		Requires: []*analysis.Analyzer{
+			analysis.InspectorAnalyzer,
+		},
+		Run: func(pass *analysis.Pass) interface{} {
+			inspector := pass.ResultOf[analysis.InspectorAnalyzer].(*ast.Inspector)
+
+			location := pass.Program.Location
+			program := pass.Program.Program
+			report := pass.Report
+
+			inspector.Preorder(
+				elementFilter,
+				func(element ast.Element) {
+
+					switchStatement, ok := element.(*ast.SwitchStatement)
+					if !ok {
+						return
+					}
+
+					duplicates := make(map[*ast.SwitchCase]bool)
+
+					duplicateChecker := NewDuplicateCaseChecker(program)
+
+					cases := switchStatement.Cases
+					for i, switchCase := range cases {
+
+						// If the current case is already identified as a duplicate,
+						// then no need to check it again. Can simply skip.
+						if _, isDuplicate := duplicates[switchCase]; isDuplicate {
+							continue
+						}
+
+						for j := i + 1; j < len(cases); j++ {
+							otherCase := cases[j]
+							if !duplicateChecker.isDuplicate(switchCase.Expression, otherCase.Expression) {
+								continue
+							}
+
+							duplicates[otherCase] = true
+
+							report(
+								analysis.Diagnostic{
+									Location: location,
+									Range:    ast.NewRangeFromPositioned(nil, otherCase.Expression),
+									Category: "lint",
+									Message:  "duplicate switch case",
+								},
+							)
+						}
+					}
+				},
+			)
+
+			return nil
+		},
+	}
+})()
+
+func init() {
+	registerAnalyzer(
+		"switch-case-analyzer",
+		SwitchCaseAnalyzer,
+	)
+}
