tromex/cadence

Improve error on missing configs

test-framework/emulator_backend.go
@@ -463,15 +463,26 @@ func (e *EmulatorBackend) replaceImports(code string) string {
 	}
 
 	sb := strings.Builder{}
-	prevLocationEnd := 0
+	locationEnd := 0
 
 	for _, importDeclaration := range program.ImportDeclarations() {
+		prevLocationEnd := locationEnd
+		locationEnd = importDeclaration.EndPos.Offset + 1
+
 		location, ok := importDeclaration.Location.(common.StringLocation)
 		if !ok {
+			// keep the import statement it as-is
+			sb.WriteString(code[prevLocationEnd:locationEnd])
 			continue
 		}
 
-		address := e.configuration.Addresses[location.String()]
+		address, ok := e.configuration.Addresses[location.String()]
+		if !ok {
+			// keep import statement it as-is
+			sb.WriteString(code[prevLocationEnd:locationEnd])
+			continue
+		}
+
 		addressStr := fmt.Sprintf("0x%s", address)
 
 		locationStart := importDeclaration.LocationPos.Offset
@@ -479,10 +490,9 @@ func (e *EmulatorBackend) replaceImports(code string) string {
 		sb.WriteString(code[prevLocationEnd:locationStart])
 		sb.WriteString(addressStr)
 
-		prevLocationEnd = importDeclaration.EndPos.Offset + 1
 	}
 
-	sb.WriteString(code[prevLocationEnd:])
+	sb.WriteString(code[locationEnd:])
 
 	return sb.String()
 }

onflow/cadence-tools

emulator_backend.go
@@ -463,26 +463,36 @@ func (e *EmulatorBackend) replaceImports(code string) string {
 	}
 
 	sb := strings.Builder{}
-	prevLocationEnd := 0
+	importDeclEnd := 0
 
 	for _, importDeclaration := range program.ImportDeclarations() {
+		prevImportDeclEnd := importDeclEnd
+		importDeclEnd = importDeclaration.EndPos.Offset + 1
+
 		location, ok := importDeclaration.Location.(common.StringLocation)
 		if !ok {
+			// keep the import statement it as-is
+			sb.WriteString(code[prevImportDeclEnd:importDeclEnd])
 			continue
 		}
 
-		address := e.configuration.Addresses[location.String()]
+		address, ok := e.configuration.Addresses[location.String()]
+		if !ok {
+			// keep import statement it as-is
+			sb.WriteString(code[prevImportDeclEnd:importDeclEnd])
+			continue
+		}
+
 		addressStr := fmt.Sprintf("0x%s", address)
 
 		locationStart := importDeclaration.LocationPos.Offset
 
-		sb.WriteString(code[prevLocationEnd:locationStart])
+		sb.WriteString(code[prevImportDeclEnd:locationStart])
 		sb.WriteString(addressStr)
 
-		prevLocationEnd = importDeclaration.EndPos.Offset + 1
 	}
 
-	sb.WriteString(code[prevLocationEnd:])
+	sb.WriteString(code[importDeclEnd:])
 
 	return sb.String()
 }