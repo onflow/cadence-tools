0b3dd2912bac5bc2844428ca47294d4346956690
Bastian Müller <bastian@turbolent.com>
2025-08-06 15:32:54 -0700

    add documentation link as code description, and migration note (if any)

--- languageserver/server/server.go
+++ languageserver/server/server.go
@@ -2398,6 +2398,28 @@ func (s *Server) convertError(
 		}
 	}
 
+	// TODO: replace with errors.HasDocumentationLink, once available
+	if hasDocumentationLink, ok := err.(interface{ DocumentationLink() string }); ok {
+		// TODO: once Cadence errors have codes, use the code instead of a placeholder
+		diagnostic.Code = "documentation"
+		diagnostic.CodeDescription = &protocol.CodeDescription{
+			Href: hasDocumentationLink.DocumentationLink(),
+		}
+	}
+
+	// TODO: replace with errors.HasMigrationNote, once available
+	if hasMigrationNote, ok := err.(interface{ MigrationNote() string }); ok {
+		// If the message does not end with a newline,
+		// add one to separate the migration note from the message
+		if !strings.HasSuffix(diagnostic.Message, "\n") {
+			diagnostic.Message += "\n"
+		}
+		diagnostic.Message += fmt.Sprintf(
+			"\nMigration note: %s",
+			hasMigrationNote.MigrationNote(),
+		)
+	}
+
 	return diagnostic, codeActionsResolver
 }
 
