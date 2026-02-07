140034c7ea8ef481308cb8be9a2c2c66ae42afd2
Bastian Müller <bastian@turbolent.com>
2026-02-06 19:19:29 -0800

    add a new analyzer to detect unused imports

--- lint/analyzer.go
+++ lint/analyzer.go
@@ -31,6 +31,7 @@ const (
 	UnnecessaryCastCategory           = "unnecessary-cast-hint"
 	UnnecessaryTypeAnnotationCategory = "unnecessary-type-annotation-hint"
 	UnusedResultCategory              = "unused-result-hint"
+	UnusedImportCategory              = "unused-import-hint"
 	DeprecatedCategory                = "deprecated"
 	CadenceV1Category                 = "cadence-v1"
 	SecurityCategory                  = "security"
